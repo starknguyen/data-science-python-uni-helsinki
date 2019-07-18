71e6af806d062c90a7f9f309aba3a4ff5305eed1
starknguyen <ngdinhnghi@gmail.com>
2019-07-18 19:36:25 +0300

    Solve simple column comparison problem

---
+++ DataScienceUniHelsinki/Week03/column_comparison.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def column_comparison(a):
+    return a[a[:,1] > a[:,-2]]
+
+
+def main():
+    a = np.array([[8, 9, 3, 8, 8], [0, 5, 3, 9, 9], [5, 7, 6, 0, 4]])
+    print(column_comparison(a))
+
+
+if __name__ == "__main__":
+    main()
