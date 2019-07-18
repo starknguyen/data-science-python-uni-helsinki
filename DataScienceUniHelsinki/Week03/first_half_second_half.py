d51d26478b24db45c2be0a0304b5b5ebb0c1b125
starknguyen <ngdinhnghi@gmail.com>
2019-07-18 22:37:32 +0300

    Solve the tricky first half second half

---
+++ DataScienceUniHelsinki/Week03/first_half_second_half.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def first_half_second_half(a):
+    half_length = int(a.shape[1] / 2)
+    return a[np.sum(a[:, :half_length], axis=1) > np.sum(a[:, half_length:], axis=1)]
+
+
+def main():
+    a = np.array([[1, 3, 4, 2],
+                 [2, 2, 1, 2]])
+    print(first_half_second_half(a))
+
+
+if __name__ == "__main__":
+    main()
